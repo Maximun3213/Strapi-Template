ed5538041277edfb3e0e8237b26a89c3d1350c95
max <max@bvm.network>
2025-08-22 15:53:22 +0700

    first commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,22 @@
-export default () => ({});
+export default () => ({
+  seo: {
+    enabled: true,
+  },
+  documentation: {
+    enabled: true,
+    config: {
+      info: {
+        title: "My API",
+        description: "API documentation with Swagger / OpenAPI 3",
+        version: "1.0.0",
+      },
+      servers: [
+        { url: "http://localhost:1337/api", description: "Local server" },
+      ],
+      externalDocs: {
+        description: "Find more info here",
+        url: "https://docs.myapi.com",
+      },
+    },
+  },
+});
---
+++ docs/config.md
@@ -0,0 +1,4 @@
+Account: maxhuynh.work@gmail.com
+Password: Max12345678
+
+Swagger URL = http://localhost:1337/documentation
