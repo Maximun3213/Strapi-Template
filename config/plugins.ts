95c8f493f87cdfe9bfac3bffd9d8fed53658722c
max <max@bvm.network>
2025-08-22 16:12:16 +0700

    Add navigation and import/export plugins; update dependencies and types

--- config/plugins.ts
+++ config/plugins.ts
@@ -19,4 +19,28 @@ export default () => ({
       },
     },
   },
+  navigation: {
+    enabled: true,
+    config: {
+      additionalFields: [
+        "audience",
+        { name: "my_custom_field", type: "boolean", label: "My custom field" },
+      ],
+      contentTypes: ["api::page.page"],
+      defaultContentTypes: "api::page.page",
+      contentTypesNameFields: {
+        "api::page.page": ["title"],
+      },
+      pathDefaultFields: {
+        "api::page.page": ["slug"],
+      },
+      allowedLevels: 2,
+    },
+  },
+  "strapi-import-export": {
+    enabled: true,
+    config: {
+      contentTypes: ["api::page.page"],
+    },
+  },
 });
